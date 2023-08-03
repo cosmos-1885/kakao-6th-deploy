9e0aa384d5062b844c62f96a52b1993e80d6a453
cosmos_1885 <dmsrkql104321@gmail.com>
2023-08-03 18:16:09 +0900

    feat: update kakao-shopping

--- src/main/java/com/example/kakao/cart/CartService.java
+++ src/main/java/com/example/kakao/cart/CartService.java
@@ -61,7 +61,7 @@ public class CartService {
         List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
 
         if (cartList.isEmpty()) {
-            throw new Exception404("장바구니가 존재하지 않습니다.");
+            throw new Exception400("장바구니가 존재하지 않습니다.");
         }
         // Cart에 담긴 옵션이 3개이면, 2개는 바나나 상품, 1개는 딸기 상품이면 Product는 2개인 것이다.
         return new CartResponse.FindAllDTO(cartList);
@@ -73,7 +73,7 @@ public class CartService {
 
         // 1. 유저 장바구니에 아무것도 없으면 예외처리
         if (cartList.isEmpty()) {
-            throw new Exception404("장바구니가 존재하지 않습니다.");
+            throw new Exception400("장바구니가 존재하지 않습니다.");
         }
 
         // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
